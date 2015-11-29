ed88279271c3310822dd2db82243ba3fbf29d6cb
Kernell <kernell.it@gmail.com>
2015-11-29 18:34:23 +0300

    Добавлен предкомпелируемый заголовок (подключается сам во все .c/.cpp файлы)

---
+++ mta-mono/src/StdInc.cpp
@@ -0,0 +1,13 @@
+/*********************************************************
+*
+*  Copyright © 2013, Innovation Roleplay Engine.
+*
+*  All Rights Reserved.
+*
+*  Redistribution and use in source and binary forms,
+*  with or without modification,
+*  is permitted only for authors.
+*
+*********************************************************/
+
+#include "StdInc.h"
